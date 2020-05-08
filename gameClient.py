9607627f2553317de0130b90bbcbe20bf64a7e4f
Ansel Tessier <at9088@bard.edu>
2020-05-08 16:45:08 -0400

    started client side of game

---
+++ gameClient.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""CMSC 226 Final project: Ansel, Henning, Henry.
+
+Client side game code for networked battleship game.
+Keeps track of game state, taking updates from server,
+and sending out moves on players turn.
+
+Author: Ansel Tessier (at9088@bard.edu)
+"""
+
+import sys
+import numpy
+import socket
+
+HOST = '172.0.0.1' ## NOTE: this is using localhost for dev, must change
+
+def make_move:
+    ## TODO: take user input and send it to server
+
+def recive_update:
+    # TODO: listen to server and update local map acordingly
+
+"""main fruntion calls game and server functionality"""
+def main:
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
