458359e1361ae80bc70d453fc4130563be162923
Hazarre <henzi@LAPTOP-G0Q2Q6PQ.localdomain>
2020-05-15 18:42:49 +0800

    server protocal defined in battleship.h (completed)

--- battleship.h
+++ battleship.h
@@ -1,33 +1,58 @@
-// client status 
-#define CONNECTING_TO_SERVER
-#define MATCHING
-#define IN_GAME
-#define MY_TURN
-#define WAITING_FOR_OPPONENT
-#define GAME_OVER
 
-//after every move of each player
-//the client sends some Move data to the server
-//then the client hangs until receiving some data from the server. 
-//client -> server
+
+/* Round procedure
+Player1 sends server their Move
+Server forwards player1's Move to player2 
+Player2 processes the Move and responds with an Update to the server
+Server forwards this Update back to player1
+Player1 confirms with server that Update is completed
+Server switch the roles(status) of player1 and player2
+Start next round
+*/
+
+
+/*
+After every move of each player,
+the client sends Move data to the server in a buffer.
+Then the client hangs until receiving some data from the server. 
+
+The move data will be communicated between the server and 
+client in the following array format: 
+int move[] = {player, type, x, y, direction};
+
+At the initial game stage, the client sends the client a 
+data packet for each ship 
+*/
+#define SIZE_MOVE 5
+#define PLAYER1 1
+#define PLAYER2 2
+#define SHIP_PLACEMENT 0
+#define SEND_MISSLE 1
+#define HORIZONTAL 0
+#define VERTICAL 1
 struct Move{
-    int player; // 1 or 2 in each game, assigned by the server
-    int type; // 0 for placing ship, 1 for guessing target. 
-    int x; // 
-    int y;
+    int player; // PLAYER1 or PLAYER2
+    int type;   // SHIP_PLACEMENT or SEND_MISSLE
+    int x;      // x coordinate for the missle or the left most (smallest x coord) of ship placement
+    int y;      // y coordinate for the missle or the top (smallest y coord) of ship placement
+    int direction; // HORIZONTAL or VERTICAL
 };
 
-// The move data will be communicated between the server client in the 
-// following array formate: 
-// int move = {player, type, x, y};
 
 
-
-//before every new move, the server will send the client some info. 
-struct Update{
-    char** state;// a matrix of the game board
-    int;
-
+#define SIZE_RESULT 1
+#define MISS 0
+#define HIT 1
+#define SUNK 2
+#define GAMEOVER 3
+/*
+The Update of each round will be communicated between the server and client in the 
+following array format: 
+int update[] = {missle_result};
+*/
+struct Result{
+    int missle_result; // MISS, HIT, SUNK, or GAMEOVER
 };
 
-//
+
+
