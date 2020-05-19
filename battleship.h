dad2cc1ff65347b43a0999287c2d9205eaf9e945
Hazarre <henzi@LAPTOP-G0Q2Q6PQ.localdomain>
2020-05-20 01:31:21 +0800

    game client completed and battleship.h updated

--- battleship.h
+++ battleship.h
@@ -1,24 +1,23 @@
-
-
-/* Round procedure
+#define PORT 8080 
+/* Round procedure:
 Player1 sends server their Move
 Server forwards player1's Move to player2 
-Player2 processes the Move and responds with an Update to the server
-Server forwards this Update back to player1
-Player1 confirms with server that Update is completed
+Player2 processes the Move and responds with an Result to the server
+Server forwards this Result back to player1
+Player1 confirms with server that Result is received
 Server switch the roles(status) of player1 and player2
 Start next round
 */
 
 
-/*
+/* Move: player1 -> server
 After every move of each player,
 the client sends Move data to the server in a buffer.
 Then the client hangs until receiving some data from the server. 
 
 The move data will be communicated between the server and 
 client in the following array format: 
-int move[] = {player, type, x, y, direction};
+char move[] = {player, type, x, y, direction};
 
 At the initial game stage, the client sends the client a 
 data packet for each ship 
@@ -40,19 +39,33 @@ struct Move{
 
 
 
-#define SIZE_RESULT 1
+/*
+The Update of each round will be communicated between the server and client in the 
+following array format: 
+char update[] = {missle_result};
+*/
+#define SIZE_RESULT 2
 #define MISS 0
 #define HIT 1
 #define SUNK 2
 #define GAMEOVER 3
-/*
-The Update of each round will be communicated between the server and client in the 
-following array format: 
-int update[] = {missle_result};
-*/
+#define RECEIVED 0
+#define NOT_RECEIVED 1
 struct Result{
+    int recieved; // RECEIVED, NOT_RECEIVED
     int missle_result; // MISS, HIT, SUNK, or GAMEOVER
 };
 
 
+#define WIN 0
+#define LOST 1
 
+/* APIs to be defined
+void opponent_move(type, x, y, direction)
+calling this move would update your board based on your opponents move. 
+
+void player_move(&buffer)
+updates the game state based on your move. 
+
+void start_game(buffer)
+*/
