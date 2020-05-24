97e05aa2a861007f39eb7764d8d7a2d27862b711
Hazarre <henzi@LAPTOP-G0Q2Q6PQ.localdomain>
2020-05-24 15:11:32 +0800

    clinet.py updated

--- gen1/client.py
+++ gen1/client.py
@@ -4,10 +4,9 @@ BUFFER_SIZE = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('54.81.106.48', PORT))
-
 PLAY_GAME = True
 
 while PLAY_GAME:
     mes = s.recv(BUFFER_SIZE) 
-    move = input(mes)
+    move = raw_input(mes)
     s.sendall(move)
