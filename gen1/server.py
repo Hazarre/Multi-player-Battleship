014ae4c62a9c233fd1d3166c1dbcc7477c2fcec9
Ubuntu <ubuntu@ip-172-26-4-41.ec2.internal>
2020-05-24 06:42:21 +0000

    server done

--- gen1/server.py
+++ gen1/server.py
@@ -1,5 +1,6 @@
 from multiprocessing import Process
 import socket
+from time import sleep
 PORT = 8080
 PORT2 = 8081
 NUM_SHIPS = 3
@@ -12,17 +13,17 @@ s.listen(50)
 
 
 def game_start(p1sock, p2sock):
-    p1sock.send("You are player1")
     for i in range(NUM_SHIPS):
-        print("here1")
-        p1sock.send("Please place your # ship: ")
-        print("here2")
+        p1sock.send("Please place your number %d ship: " % i)
         move = p1sock.recv(BUFFER_SIZE)
+	print("recieved move %s from player 1" % move)
 
-    # p2sock.send("You are player2")
-    # for i in range(NUM_SHIPS):
-    #     p2sock.send("Please place you ship:")
-
+    for i in range(NUM_SHIPS):
+        p2sock.send("Please place younumber %d ship: " % i)
+        move = p2sock.recv(BUFFER_SIZE)
+        print("player2 move %s" % move)
+   
+ while
 print("waiting")
 while True:
     try:
@@ -30,8 +31,10 @@ while True:
         print("connected to player 1")
         # p2sock, p2addr = s.accept()
         # print("connected to 2 client's")
-        p = Process(target=game_start, args=(p1sock, ))
+        p = Process(target=game_start, args=(p1sock,0 ))
         p.start()
     except socket.error:
-        print('got a socket error')
-    s.close()
+        break
+        sleep(10)
+print('got a socket error')
+s.close()
