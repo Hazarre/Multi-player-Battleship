ddfe817249806352b84a476161722679ffc8843e
Hazarre <henzi@LAPTOP-G0Q2Q6PQ.localdomain>
2020-05-24 14:22:44 +0800

    a

--- gen1/server.py
+++ gen1/server.py
@@ -15,22 +15,22 @@ def game_start(p1sock, p2sock):
     p1sock.send("You are player1")
     for i in range(NUM_SHIPS):
         print("here1")
-        p1sock.send("Please place your # %d ship: " % i)
+        p1sock.send("Please place your # ship: ")
         print("here2")
-        p1sock.recv(BUFFER_SIZE)
+        move = p1sock.recv(BUFFER_SIZE)
 
-    p2sock.send("You are player2")
-    for i in range(NUM_SHIPS):
-        p2sock.send("Please place you ship:")
+    # p2sock.send("You are player2")
+    # for i in range(NUM_SHIPS):
+    #     p2sock.send("Please place you ship:")
 
 print("waiting")
 while True:
     try:
         p1sock, p1addr = s.accept()
         print("connected to player 1")
-        p2sock, p2addr = s.accept()
-        print("connected to 2 client's")
-        p = Process(target=game_start, args=(p1sock, p2sock))
+        # p2sock, p2addr = s.accept()
+        # print("connected to 2 client's")
+        p = Process(target=game_start, args=(p1sock, ))
         p.start()
     except socket.error:
         print('got a socket error')
