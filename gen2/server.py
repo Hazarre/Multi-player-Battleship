e687eabb10cd51f2aafe44b5e3e451efd24491d5
Hazarre <henzi@LAPTOP-G0Q2Q6PQ.localdomain>
2020-05-26 18:57:39 +0800

    gen2 first try2

--- gen2/server.py
+++ gen2/server.py
@@ -6,25 +6,27 @@ PORT = 8080
 BUFFER_SIZE = 1024
 
 
-def update_state(id, state):
-    psockets[id].sendall(MESSAGE_ENCODING[state])
+class Session:
+    def __init__(self, p1sock, p2sock):
+        self.psockets = [p1sock,p2sock]
+        self.g=Game()
+    def update_state(self, id, state):
+        self.psockets[id].sendall(MESSAGE_ENCODING[state])
+    def start_game():
+        while True:
+            for id in range(2):
+                # player id's turn
+                self.update_state(id,'my_turn')
+                move = self.psockets[id].recv(BUFFER_SIZE)
+                print("recieved move %s from player %d" % (move, id+1))
+                self.g.update_game(move,id)
+                if self.g.state == STATE["gameover"]:
+                    self.update_state(id, 'you_win')
+                    self.update_state((id+1)%2,'you_lost')
+                self.update_state(id, self.g.players[id].message)
 
-def game2p(p1sock, p2sock):
-    g = Game()
-    psockets = [p1sock,p2sock]
-    while True:
-        for id in range(2):
-            # player id's turn
-            update_state(id,'my_turn')
-            move = psockets[id].recv(BUFFER_SIZE)
-            print("recieved move %s from player %d" % (move, id+1))
-            g.update_game(move,id)
-            if g.state == STATE["gameover"]:
-                update_state(id, 'you_win')
-                update_state((id+1)%2,'you_lost')
-            update_state(id, g.players[id].message)
-
-
+def start_session(p1sock,p2sock):
+    Session(p1sock,p2sock).start_game()
 
 
 # multiprocess server
@@ -38,7 +40,7 @@ while True:
         print("connected to player 1")
         p2sock, p2addr = s.accept()
         print("connected to player 2")
-        p = Process(target=game2p, args=(p1sock, p2sock))
+        p = Process(target=start_session, args=(p1sock, p2sock))
         p.start()
     except socket.error:
         print('got a socket error')
