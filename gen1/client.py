b52fb8a73c7fa4fb52222973c97c58738e1ee962
Hazarre <henzi@LAPTOP-G0Q2Q6PQ.localdomain>
2020-05-27 12:15:29 +0800

    add test

--- gen1/client.py
+++ gen1/client.py
@@ -1,6 +1,10 @@
 import socket
+import  battleship
+flags = battleship.FLAGS
+flags = {v: k for k, v in flags.items()}
 PORT = 8080
 BUFFER_SIZE = 6
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('3.95.242.45', PORT))
@@ -8,7 +12,21 @@ PLAY_GAME = True
 
 while PLAY_GAME:
     print("recving")
-    mes = s.recv(BUFFER_SIZE) 
+    mes = s.recv(BUFFER_SIZE).decode("UTF-8")
+    parse_out(m)
     move = input(mes)
     s.sendall(move.encode("UTF-8"))
     print("sent mes")
+
+def parse_out(m):
+    for i in range(len(m)):
+        msg = m[i]
+        if len(msg) == 2:
+            msg[0] = flags[msg[0]]
+        elif len(msg) == 3:
+            if msg[0]:
+                msg = 'you were hit at '+str((msg[1],msg[2]))
+            else:
+                msg = 'op missed at '+str((msg[1],msg[2]))
+        m[i] = msg
+    return m
