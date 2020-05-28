c24811368a73c90f619fdb53888b0a599d6a5674
Ansel Tessier <at9088@bard.edu>
2020-05-28 02:54:40 -0400

    got threaded version of client working, should be good, still need to be tested with server

--- gameClient.py
+++ gameClient.py
@@ -20,7 +20,7 @@ HOST = '127.0.0.1' ## NOTE: this is using localhost for dev, must change
 PORT = 65432
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.setblocking(0)
+#s.setblocking(0)
 
 #setting up threading
 c = threading.Condition()
@@ -40,11 +40,11 @@ game_play = True
 last_shot = (-1,-1) #cords and bool hit (0=false)
 
 flagdict = {
-    1 : "Wait your turn"
-    2 : "Waiting for other player to place ships"
-    3 : "Other player ready"
-    4 : "placed a ship" #see communications.txt
-    5 : "Repeated fire"
+    1 : "Wait your turn",
+    2 : "Waiting for other player to place ships",
+    3 : "Other player ready",
+    4 : "placed a ship", #see communications.txt
+    5 : "Repeated fire",
     6 : "GAME OVER"#see communications.txt
 }
 
@@ -60,6 +60,7 @@ class listener(threading.Thread):
     def run(self):
         global enemyboard
         global myboard
+        global game_play
         global gameflag
         global threadflag
         global last_shot
@@ -75,7 +76,7 @@ class listener(threading.Thread):
                 if(message[0] == 0):
                     gameflag = flagdict.get(message[1])
                 elif(message[0] == 1):
-                    if(message[1] = 0):
+                    if(message[1] == 0):
                         enemyboard[last_shot[2]][last_shot[1]] = 0
                     else:
                         enemyboard[last_shot[2]][last_shot[1]] = 2
@@ -89,118 +90,151 @@ class listener(threading.Thread):
                 c.wait()
             c.release()
 
+"""game_core class makes a thread that deals with all local game logic
+   and rendering."""
+class game_core(threading.Thread):
+    def __init__(self, name):
+        threading.Thread.__init__(self)
+        self.name = name
+    def run(self):
+        curses.wrapper(self.display)
 
-#sends your move to the server as a byte object
-def make_move(pos):
-    target = bytes(str(pos),'utf-8')
-    s.sendall(target)
+        #while game_play:
+        #    c.acquire()
+        #    if threadflag == 1:
+        #        # TODO: migrate exsisting code here
+        #        threadflag == 0
+        #        c.notify_all()
+        #    else:
+        #        c.wait()
+        #    c.release()
 
-# TODO: s.recv dosen't play well with sockets
-def recive_update():
-    myboard = s.recv(4096)
 
-    # TODO: look for message that switches game state
-    #may need to convert update from bytes to numpyarray
 
-#renders game to terminal
-def draw_board(stdscr):
-    print("here")
-    k = 0
-    cursor_x = 0
-    cursor_y = 0
+    #renders game to terminal
+    def display(self, stdscr):
 
-    # Clear and refresh the screen for a blank canvas
-    stdscr.clear()
-    stdscr.refresh()
+        global enemyboard
+        global myboard
+        global game_play
+        global gameflag
+        global threadflag
+        global last_shot
+        global s
 
-    # Start colors in curses
-    curses.start_color()
-    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
-
-    # Loop where k is the last character pressed
-    while (k != ord('q')):
-
-        # Initialization
+        self.k = 0
+        self.cursor_x = 0
+        self.cursor_y = 0
+        # Clear and refresh the screen for a blank canvas
         stdscr.clear()
-        height, width = stdscr.getmaxyx()
-
-        if k == curses.KEY_DOWN:
-            cursor_y = cursor_y + 2
-        elif k == curses.KEY_UP:
-            cursor_y = cursor_y - 2
-        elif k == curses.KEY_RIGHT:
-            cursor_x = cursor_x + 4
-        elif k == curses.KEY_LEFT:
-            cursor_x = cursor_x - 4
-
-        cursor_x = max(0, cursor_x)
-        cursor_x = min(width-1, cursor_x)
-
-        cursor_y = max(0, cursor_y)
-        cursor_y = min(height-1, cursor_y)
-
-        if k == ord(' '):
-            make_move((cursor_x//4, cursor_y//2))
-            last_shot = (cursor_x//4, cursor_y//2)
-
-        #status bar
-        statusbarstr = "Press 'q' to exit | Target with arrow keys, fire with space | Target: {}, {}".format(cursor_x//4, cursor_y//2)
-        stdscr.attron(curses.color_pair(3))
-        stdscr.addstr(height-1, 0, statusbarstr)
-        stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
-        stdscr.attroff(curses.color_pair(3))
-
-        stdscr.attron(curses.A_BOLD)
-        for i in range(0,9): #rendering boards
-            for j in range(0,9):
-                try:
-                    if(enemyboard[i][j] == 0): #drawing water
-                        stdscr.attron(curses.color_pair(1))
-                        stdscr.addch(i*2, j*4, 'o')
-                        stdscr.attroff(curses.color_pair(1))
-                    elif(enemyboard[i][j] == 1): #drawing unhit ship
-                        stdscr.attron(curses.color_pair(3))
-                        stdscr.addch(i*2, j*4, 'x')
-                        stdscr.attroff(curses.color_pair(3))
-                    elif(enemyboard[i][j] == 2): #drawing hit ship
-                        stdscr.attron(curses.color_pair(2))
-                        stdscr.addch(i*2, j*4, 'x')
-                        stdscr.attroff(curses.color_pair(2))
-
-                    if(myboard[i][j] == 0):
-                        stdscr.attron(curses.color_pair(1))
-                        stdscr.addch((i+10)*2, j*4, ord('o'))
-                        stdscr.attroff(curses.color_pair(1))
-                    elif(myboard[i][j] == 1):
-                        stdscr.attron(curses.color_pair(3))
-                        stdscr.addch((i+10)*2, j*4, 'x')
-                        stdscr.attroff(curses.color_pair(3))
-                    elif(myboard[i][j] == 2):
-                        stdscr.attron(curses.color_pair(2))
-                        stdscr.addch((i+10)*2, j*4, 'x')
-                        stdscr.attroff(curses.color_pair(2))
-
-                except(curses.error):
-                    pass
-
-        stdscr.attroff(curses.A_BOLD)
-
-        stdscr.move(cursor_y, cursor_x)
-        # Refresh the screen
         stdscr.refresh()
+        # Start colors in curses
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        # Loop where k is the last character pressed
+        while (self.k != ord('q')):
+            print('hello', self.k)
+            #updating instance vars from listener theread
+        #    if threadflag == 1:
+                # TODO: migrate exsisting code here
 
-        # Wait for next input
-        k = stdscr.getch()
+        #        threadflag == 0
+        #        c.notify_all()
+        #    else:
+        #        c.wait()
+        #    c.release()
 
-        #recive_update()
+            # Initialization
+            stdscr.clear()
+            self.height, self.width = stdscr.getmaxyx()
+
+            if self.k == curses.KEY_DOWN:
+                self.cursor_y = self.cursor_y + 2
+            elif self.k == curses.KEY_UP:
+                self.cursor_y = self.cursor_y - 2
+            elif self.k == curses.KEY_RIGHT:
+                self.cursor_x = self.cursor_x + 4
+            elif self.k == curses.KEY_LEFT:
+                self.cursor_x = self.cursor_x - 4
+
+            self.cursor_x = max(0, self.cursor_x)
+            self.cursor_x = min(self.width-1, self.cursor_x)
+            self.cursor_y = max(0, self.cursor_y)
+            self.cursor_y = min(self.height-1, self.cursor_y)
+
+            if self.k == ord(' '):
+                s.sendall(bytes(str((self.cursor_x//4, self.cursor_y//2)),'utf-8'))
+                last_shot = (self.cursor_x//4, self.cursor_y//2)
+
+            #status bar
+            self.statusbarstr = "Press 'q' to exit | Target with arrow keys, fire with space | Target: {}, {}".format(self.cursor_x//4, self.cursor_y//2)
+            stdscr.attron(curses.color_pair(3))
+            stdscr.addstr(self.height-1, 0, self.statusbarstr)
+            stdscr.addstr(self.height-1, len(self.statusbarstr), " " * (self.width - len(self.statusbarstr) - 1))
+            stdscr.attroff(curses.color_pair(3))
+
+            stdscr.attron(curses.A_BOLD)
+
+            #c.acquire()
+            #if(threadflag == 1):
+            for i in range(0,9): #rendering boards
+                for j in range(0,9):
+                    try:
+                        if(enemyboard[i][j] == 0): #drawing water
+                            stdscr.attron(curses.color_pair(1))
+                            stdscr.addch(i*2, j*4, 'o')
+                            stdscr.attroff(curses.color_pair(1))
+                        elif(enemyboard[i][j] == 1): #drawing unhit ship
+                            stdscr.attron(curses.color_pair(3))
+                            stdscr.addch(i*2, j*4, 'x')
+                            stdscr.attroff(curses.color_pair(3))
+                        elif(enemyboard[i][j] == 2): #drawing hit ship
+                            stdscr.attron(curses.color_pair(2))
+                            stdscr.addch(i*2, j*4, 'x')
+                            stdscr.attroff(curses.color_pair(2))
+
+                        if(myboard[i][j] == 0):
+                            stdscr.attron(curses.color_pair(1))
+                            stdscr.addch((i+10)*2, j*4, ord('o'))
+                            stdscr.attroff(curses.color_pair(1))
+                        elif(myboard[i][j] == 1):
+                            stdscr.attron(curses.color_pair(3))
+                            stdscr.addch((i+10)*2, j*4, 'x')
+                            stdscr.attroff(curses.color_pair(3))
+                        elif(myboard[i][j] == 2):
+                            stdscr.attron(curses.color_pair(2))
+                            stdscr.addch((i+10)*2, j*4, 'x')
+                            stdscr.attroff(curses.color_pair(2))
+
+                    except(curses.error):
+                        pass
+            #else:
+            #    c.wait()
+            #c.release()
+
+            stdscr.attroff(curses.A_BOLD)
+
+            stdscr.move(self.cursor_y, self.cursor_x)
+            # Refresh the screen
+            stdscr.refresh()
+
+            # Wait for next input
+            self.k = stdscr.getch()
+
+
 
 
 
 """main fruntion calls game and server functionality"""
 def main():
-    curses.wrapper(draw_board)
+    l = listener("listener_thread")
+    g = game_core("game_thread")
+    l.start()
+    g.start()
+    l.join()
+    g.join()
 
 
 if __name__ == "__main__":
